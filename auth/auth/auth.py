252e9699fbf370dd8d1424c465a981d178471391
gamitej <gamitejpratapsingh998@gmail.com>
2022-06-17 11:10:36 +0530

    auth

--- auth/auth/auth.py
+++ auth/auth/auth.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,jsonify
 from flask import Blueprint
-from db import getUser
+from db import getUser,addUser
 
 auth = Blueprint('auth', __name__)
 
@@ -8,8 +8,23 @@ auth = Blueprint('auth', __name__)
 def login():
     try:
         req = request.get_json()
+        if 'user' not in req or 'passwd' not in req:
+            return jsonify({"msg":'Bad Request'}),400
         user,passwd = req['user'],req['passwd'] 
         res = getUser(user,passwd)
         return res
     except Exception as e:
         return jsonify({"msg":'Error Occured'}),500
+
+@auth.route('/signup',methods=['POST'])
+def signup():
+    try:
+        req = request.get_json()
+        if 'user' not in req or 'passwd' not in req:
+            return jsonify({"msg":'Bad Request'}),400
+        user,passwd = req['user'],req['passwd'] 
+        res = addUser(user,passwd)
+        return res
+    except Exception as e:
+        return jsonify({"msg":'Error Occured'}),500
+        
--- auth/db.py
+++ auth/db.py
@@ -8,3 +8,9 @@ def getUser(userName,passwd):
             return jsonify({"msg":'Login Successfull'}),200
         return jsonify({"msg":'Password Incorrect'}),401
     return jsonify({"msg":'Username Incorrect'}),401
+
+def addUser(userName,passwd):
+    if userName in userDb:
+        return jsonify({"msg":"User already exist"}),409
+    userDb[userName] = passwd
+    return jsonify({"msg":"SignUp SuccessFull"}),200
