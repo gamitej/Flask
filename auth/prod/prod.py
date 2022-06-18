88b5b1e9b1ca2f018cf5a818ce271eada5b93881
gamitej <gamitejpratapsingh998@gmail.com>
2022-06-18 22:52:16 +0530

    prod.py

--- auth/prod/prod.py
+++ auth/prod/prod.py
@@ -1,18 +1,21 @@
 from flask import Blueprint
+from flask import Flask,jsonify
 from db import prodAvail,prodById,allProd
 
 prod = Blueprint('prod', __name__)
 
 @prod.route('/')
 def prodList():
-    res = 
-    return allProd()
+    res = allProd()
+    return jsonify(res),200
 
 @prod.route('/<int:id>')
 def prodId(id):
-    return prodById(id)
+    res = prodById(id)
+    return jsonify(res)
 
 @prod.route('/<string:name>')
 def prodName(name):
-    return prodAvail(name)
+    res = prodAvail(name)
+    return jsonify(res)
 
