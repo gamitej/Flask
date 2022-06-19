1da6e3d7c87c5ff018793526231a59eb08075794
gamitej <gamitejpratapsingh998@gmail.com>
2022-06-19 13:07:57 +0530

    token set db.py

--- Token/normal/db.py
+++ Token/normal/db.py
@@ -1,10 +1,20 @@
 import uuid 
+from datetime import datetime
 
 userDb = {"amitej":'1234',"singh":'4321'}
 
-userToken = {"amitej":'',"singh":''}
+userToken = {"amitej":{"status":False},"singh":{"status":False}}
 
 
-def authUser():
+def authUser(user,passwd):
+    if user in userDb:
+        if userDb[user] == passwd:
+            token = uuid.uuid1().hex
+            userToken[user] = {"status":True,"token":token,"time":datetime.now()}
+            return True,{"token":token} 
+        return False,{"msg":"Incorrect Password"} 
+    return False,{"msg":"Username not found"}
+
+def tokenCheck():
     return ''
 
