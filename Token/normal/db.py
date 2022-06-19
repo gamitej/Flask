c95c68672e99e86d6d9c84811467c9561923064f
gamitej <gamitejpratapsingh998@gmail.com>
2022-06-19 13:31:03 +0530

    token in server.py

--- Token/normal/db.py
+++ Token/normal/db.py
@@ -5,16 +5,26 @@ userDb = {"amitej":'1234',"singh":'4321'}
 
 userToken = {"amitej":{"status":False},"singh":{"status":False}}
 
+tokenUser = {}
 
 def authUser(user,passwd):
     if user in userDb:
         if userDb[user] == passwd:
             token = uuid.uuid1().hex
             userToken[user] = {"status":True,"token":token,"time":datetime.now()}
+            tokenUser[token] = user
             return True,{"token":token} 
         return False,{"msg":"Incorrect Password"} 
     return False,{"msg":"Username not found"}
 
-def tokenCheck():
-    return ''
+def tokenCheck(token,time):
+    if token not in tokenUser:
+        return False,{"msg":"Token Not Found"}
+    user = tokenUser[token]
+    if user in userToken and userToken[user]["status"] and userToken[user]["token"] == token:
+        timeDiff = time - userToken[user]["time"]
+        if timeDiff.seconds <= 300:
+            return True,{"msg":"Success"} 
+        return False,{"msg":"Token Expired"} 
+    return False,{"msg":"Token Invalid"}
 
