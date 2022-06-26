11813d0f79034efe26f70748bf9b10f4ad9df49e
gamitej <gamitejpratapsingh998@gmail.com>
2022-06-26 09:48:52 +0530

    token sqlite3

---
+++ Token/sqlite3_token_gen/sql_server.py
@@ -0,0 +1,37 @@
+import sqlite3
+from venv import create
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+create_table = '''
+        CREATE TABLE IF NOT EXISTS users (
+            user_id text,
+            firstname VARCHAR(25) NOT NULL,
+            lastname VARCHAR(25) NOT NULL,
+            PRIMARY KEY (user_id)
+        )
+        ''' 
+cursor.execute(create_table)
+
+create_table = '''
+        CREATE TABLE IF NOT EXISTS users_token (
+            user_id text,
+            token VARCHAR(25) NOT NULL,
+            expire_time int NOT NULL,
+            PRIMARY KEY (user_id),
+            CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
+        )
+        ''' 
+cursor.execute(create_table)
+
+#---------- INSERT MANY ROWS --------------
+
+users = [ (1,'amitej','1234'),(2,'rooney','2211'),(3,'singh','4321')]
+
+insert_query = "INSERT OR IGNORE INTO users VALUES(?,?,?)"
+
+cursor.executemany(insert_query,users)
+
+connection.commit()
+connection.close()
