987e48fef6e9c40b239b3806f481811ea04bc382
gamitej <gamitejpratapsingh998@gmail.com>
2022-06-26 17:25:29 +0530

    token common select command

--- Token/sqlite3_token_gen/user.py
+++ Token/sqlite3_token_gen/user.py
@@ -5,13 +5,19 @@ from datetime import date, datetime,timedelta
 def connect_to_db():
     connection = sqlite3.connect('data.db')
     return connection
-    
+
+def fetchOneRow(value,rows,where_cond_row):
+    cursor = connect_to_db()
+    query = f"select {rows} from users where {where_cond_row}=?"
+    result =  cursor.execute(query,(value,))
+    row = result.fetchone()
+    return row
+
 def authUser(username,password):
     connection = connect_to_db()
     cursor = connection.cursor()
-    query = "select user_id,password from users where username=?"
-    result =  cursor.execute(query,(username,))
-    row = result.fetchone()
+    rows = "user_id,password"
+    row = fetchOneRow(username,rows,"username")
     if row:
         user_id,passwd = row[0],row[1]
         if passwd == password:
@@ -35,18 +41,20 @@ def tokenCheck(token,time,reqRoute):
     row = result.fetchone()
     if not row:
         return False,{"msg":"Token Not Found"}
-    # ----- To convert into datetime format
+    # ----- To convert into datetime format -------
     expire_time = datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S.%f') 
     timeDiff = time - expire_time
     if timeDiff.seconds <= 120:
         # for requested route we will increase the expire time by 3 min
         if reqRoute:
+            # ---------- UPDATE THE TOKEN EXPIRE TIME ------------
             update_query = "UPDATE users_token SET expire_time = ? where token = ?"
             new_expire_time = expire_time + timedelta(minutes=3)
             update = (new_expire_time,token)
             cursor.execute(update_query,update)
             connection.commit()
             connection.close()
+            # ---------- UPDATE THE TOKEN EXPIRE TIME ------------
         return True,{"msg":"Success"} 
     return False,{"msg":"Token Expired Please Login Again"} 
 
